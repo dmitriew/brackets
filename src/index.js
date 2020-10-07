2b1a179d083a16d4664595c52d1592700f7f5186
dmitriew <hjq3301@mail.ru>
2020-10-07 19:30:53 +0700

    feat: solution for brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array = [];
+
+  if (str.length % 2 !== 0) {
+    return false;
+  } else {
+    for (let i = 0; i < str.length; i++) {
+      for (let item of bracketsConfig) {
+        if (str[i] == item[1] && array[array.length - 1] == item[0]) {
+          array.pop();
+        }
+        else if (str[i] == item[0]) {
+          array.push(str[i]);
+        }
+      }
+    }
+  }
+  if (array.length == 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
+// check('())(', [['(', ')']])
